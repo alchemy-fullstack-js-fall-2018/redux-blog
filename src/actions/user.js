7631d5eb00350a30071f9a971b0410ff504e687b
Davidchhing <davidhchhing@gmail.com>
2018-11-30 10:21:17 -0800

    Completed users actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,16 +1,13 @@
 import { getUsers } from '../services/blogService';
 
-
+export const USERS_LOAD_START = 'USERS_LOAD_START';
+export const USERS_LOAD_END = 'USERS_LOAD_END';
 export const FETCH_USERS = 'FETCH_USERS';
-export const fetchUsers = ( name, username, email, address, phone ) => {
+export const fetchUsers = ( searchTerm ) => {
   return {
     type: FETCH_USERS,
-    payload: {
-      name,
-      username,
-      email,
-      address,
-      phone
-    }
+    loadStart: USERS_LOAD_START,
+    loadEnd: USERS_LOAD_END,
+    payload: getUsers(searchTerm)
   };
 };
