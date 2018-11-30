f5ac82632dd5302ee7bddb79efa2955450582607
KLPainter <KLPainter@gmail.com>
2018-11-29 16:17:03 -0800

    initial setup compiles

---
+++ src/components/app/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+// import ROUTES from '../../routes';
+
+export default function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Switch>
+
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+}
