60574325d2a2d4cec0aeeaa511423166952a8cf8
Ryan Luras <ryanluras@gmail.com>
2018-11-30 09:55:23 -0800

    setting up environment

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from '../Header/Header';
+// import { ROUTES } from '../../routes/routes';
+
+export default function App() {
+  return (
+    // <Router>
+      <Fragment>
+        <Header />
+        {/* <Switch> */}
+
+        {/* </Switch> */}
+      </Fragment>
+    // </Router>
+  );
+}
