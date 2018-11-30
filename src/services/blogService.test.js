0065cfdae3e79d226c9e080a41b41df0ec2bff70
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-11-29 17:34:48 -0800

    first api test passing

---
+++ src/services/blogService.test.js
@@ -0,0 +1,35 @@
+import mockUsers from './fixtures/users.json';
+import mockPosts from './fixtures/posts.json';
+import mockComments from './fixtures/comments.json';
+
+import { getUsers, getPosts, getComments } from './blogService';
+
+jest.mock('./request.js', () => ({
+  get: url => {
+    if(url.startsWith('https://jsonplaceholder.typicode.com/users')) {
+      return Promise.resolve(mockUsers);
+    } else if(url.startsWith('https://jsonplaceholder.typicode.com/posts')) {
+      return Promise.resolve(mockPosts);
+    } else if(url.startsWith('https://jsonplaceholder.typicode.com/comments?postId=')) {
+      return Promise.resolve(mockComments);
+    } else {
+      return Promise.reject({ error: '404' });
+    }
+  }
+}));
+
+describe('blogService', () => {
+  it('gets a list of Users with name, username, email and id', () => {
+    return getUsers()
+      .then(users => {
+        users.users.forEach(user => {
+          expect(user).toEqual({
+            name: expect.any(String),
+            email: expect.any(String),
+            username: expect.any(String),
+            id: expect.any(Number),
+          });
+        });
+      });
+  });
+});
