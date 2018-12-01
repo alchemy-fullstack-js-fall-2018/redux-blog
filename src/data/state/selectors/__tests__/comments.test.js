851cb18f38dd091af20a507dd7cddfaf4261e2d3
Jack <ajackofsometrades@gmail.com>
2018-11-30 16:25:10 -0800

    comments and users selectors are goode

---
+++ src/data/state/selectors/__tests__/comments.test.js
@@ -0,0 +1,17 @@
+import { getComments } from '../comments';
+import { mockComments } from '../../../../testing/fixtures/comments';
+
+
+describe('comment selector', () => {
+  const state = {
+    comments: {
+      list: mockComments,
+      loading: false
+    }
+  };
+
+  test('gets comment list from state', () => {
+    expect(getComments(state)).toEqual(mockComments);
+  });
+
+});
---
+++ src/data/state/selectors/__tests__/users.test.js
@@ -0,0 +1,17 @@
+import { getUsers } from '../users';
+import { mockUsers } from '../../../../testing/fixtures/users';
+
+
+describe('user selector', () => {
+  const state = {
+    users: {
+      list: mockUsers,
+      loading: false
+    }
+  };
+
+  test('gets user list from state', () => {
+    expect(getUsers(state)).toEqual(mockUsers);
+  });
+
+});
