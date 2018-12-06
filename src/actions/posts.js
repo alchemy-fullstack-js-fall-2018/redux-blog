a95d7fe2b38418c1af97bd81711cc809a52e5854
Sarah Flynn <sarah.flynn@icloud.com>
2018-12-05 17:48:26 -0800

    start and done actions added for comments, posts, and users

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,8 +1,12 @@
 import { getPosts, getPost } from '../services/jsonPlaceholderApi';
 
+export const FETCH_POSTS_LOADING = 'FETCH_POSTS_START';
+export const FETCH_POSTS_DONE = 'FETCH_POSTS_DONE';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const fetchPosts = () => ({
   type: FETCH_POSTS,
+  loadStart: FETCH_POSTS_LOADING,
+  loadEnd: FETCH_POSTS_DONE,
   payload: getPosts()
 });
 
--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,12 @@
 import { getUsers, getUser } from '../services/jsonPlaceholderApi';
 
+export const FETCH_USERS_LOADING = 'FETCH_USERS_START';
+export const FETCH_USERS_DONE = 'FETCH_USERS_DONE';
 export const FETCH_USERS = 'FETCH_USERS';
 export const fetchUsers = () => ({
   type: FETCH_USERS,
+  loadStart: FETCH_USERS_LOADING,
+  loadEnd: FETCH_USERS_DONE,
   payload: getUsers()
 });
 
